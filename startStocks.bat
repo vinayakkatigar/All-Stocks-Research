5bbfc3c02875a45b3a18864b45366d77770dcd12
Vinayak Katigar <vinayak.katigar.external@atos.net>
2023-01-09 15:40:25 +0000

    09012023 Fix

--- startStocks.bat
+++ startStocks.bat
@@ -1,10 +1,22 @@
-cd C:\Code_Base\All-Stocks-Research
+rmdir /S /Q %temp%
+
+cd C:\Code\All-Stocks-Research
 c:
 
 c:\
 c:/
-cd C:\Code_Base\All-Stocks-Research
+cd C:\Code\All-Stocks-Research
 
-rmdir /S /Q C:\Code_Base\All-Stocks-Research\logs
+rmdir /S /Q C:\Code\All-Stocks-Research\logs
+
+mkdir C:\Users\Vin\AppData\Local\Temp\
+
+git reset --hard HEAD
+git pull
+git reset --hard HEAD
+git pull
+git reset --hard HEAD
+git pull
+
 
 mvn clean spring-boot:run
