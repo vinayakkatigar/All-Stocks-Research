e3397fafe237057ebe73e5fbc21270c5b4c0316f
Vinayak Katigar <vinayak.katigar.external@atos.net>
2023-04-16 18:55:02 +0100

    16042023

---
+++ src/main/java/stock/research/entity/dto/StockDetails.java
@@ -0,0 +1,25 @@
+package stock.research.entity.dto;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "STOCK_DETAILS")
+public class StockDetails {
+
+    // "customer_seq" is Oracle sequence name.
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "CUST_SEQ")
+    @SequenceGenerator(sequenceName = "customer_seq", allocationSize = 1, name = "CUST_SEQ")
+    Long id;
+
+    String name;
+
+    String email;
+
+    @Column(name = "UPDATED_TS")
+    Timestamp uppdatedTS;
+
+    //getters and setters, contructors
+
+}
