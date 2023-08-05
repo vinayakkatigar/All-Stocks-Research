a480e2f3c44753b8a57ae473356449b97adc41ed
vinayakkatigar <47253535+vinayakkatigar@users.noreply.github.com>
2023-08-05 11:11:47 +0100

    01082023 bat script  Fix

--- gFinanceAlive.bat
+++ gFinanceAlive.bat
@@ -1,12 +1,12 @@
 :again
 start chrome https://docs.google.com/spreadsheets/d/1r0ZqMeOPIfkoakhcW3dGHE2YsKgJJO4M7InwgcP2-Ao/edit#gid=0
-timeout /t 10 /nobreak > NUL
+timeout /t 30 /nobreak > NUL
 start chrome https://docs.google.com/spreadsheets/d/1DdkJYnXIR0UCLeB7cjB8G4LGzZMXHKQ_dGpXGO8CU8I/edit#gid=0
-timeout /t 10 /nobreak > NUL
+timeout /t 30 /nobreak > NUL
 start chrome https://docs.google.com/spreadsheets/d/1YmgSZuLMPJqgPLUuaQCTq2TXjts4aPD0w19zYpV0WpE/edit#gid=0
-timeout /t 10 /nobreak > NUL
+timeout /t 30 /nobreak > NUL
 start chrome https://docs.google.com/spreadsheets/d/1TGdtwdz_6O9wTO3xFzUwo4wlbsyGsQWeHolLVJzxLyQ/edit#gid=0
-timeout /t 10 /nobreak > NUL
+timeout /t 30 /nobreak > NUL
 start chrome https://docs.google.com/spreadsheets/d/1M7swFwopiNGRZn052yCc1YM3cYVQvLiBytJNehncSPI/edit#gid=0
 timeout /t 600 /nobreak > NUL
 TASKKILL /IM chrome.exe /F
