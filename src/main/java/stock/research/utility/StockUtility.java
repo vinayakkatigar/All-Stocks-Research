acd5908deb629e557ecc2e413252c8d233cf477b
Vin Kati <Raghu.kat@outlook.com>
2023-11-12 10:22:24 +0000

    Fix

--- src/main/java/stock/research/utility/StockUtility.java
+++ src/main/java/stock/research/utility/StockUtility.java
@@ -1,9 +1,12 @@
 package stock.research.utility;
 
+import java.time.Instant;
+
 public class StockUtility {
 
     public static void goSleep(int x){
         try {
+            System.out.println(Instant.now() + ", starting goSleep for: " + x);
             Thread.sleep(1000 * x);
         } catch (InterruptedException e) { }
     }
