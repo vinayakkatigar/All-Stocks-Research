8940d37a001828e96c6de02be65a913ef4be4515
vinayakkatigar <47253535+vinayakkatigar@users.noreply.github.com>
2024-08-14 19:43:08 +0100

    14082024 GF Fix

--- gFinanceAlive.bat
+++ gFinanceAlive.bat
@@ -69,7 +69,15 @@ timeout /t 60 /nobreak > NUL
 start chrome https://docs.google.com/spreadsheets/d/1Sr8VyB61Lu5GYMOrceNbwM7WTyDuaUPOD3wTCwXBnNk/edit?gid=0#gid=0
 timeout /t 60 /nobreak > NUL
 start chrome https://docs.google.com/spreadsheets/d/1_5CTJjqt6AEbzyE2EjvXNoshnie3dxGtX09Pspxto3A/edit?gid=0#gid=0
-timeout /t 240 /nobreak > NUL
+timeout /t 60 /nobreak > NUL
+start chrome https://docs.google.com/spreadsheets/d/1tLDyMvoIR8tUmMjKSGWAGKSTYyQfHG50xZQNG1gVXGU/edit?gid=0#gid=0
+timeout /t 60 /nobreak > NUL
+start chrome https://docs.google.com/spreadsheets/d/1p1wCt1zbz8b6tVEImuhpmooDiiPLsIDLxUdJZljeq3A/edit?gid=0#gid=0
+timeout /t 60 /nobreak > NUL
+start chrome https://docs.google.com/spreadsheets/d/1FRliqkfwDvF5969jW0tybnn6QIsWmC70jlS7oNPDh7c/edit?gid=0#gid=0
+timeout /t 60 /nobreak > NUL
+start chrome https://docs.google.com/spreadsheets/d/1HsF63N9SZd8ByxGOvidy4XCOkRRpmD5QaEs49x0T2Ro/edit?gid=0#gid=0
+timeout /t 60 /nobreak > NUL
 
 start chrome --new-window http://localhost:9090/gf/nyse/wnl
 timeout /t 30 /nobreak > NUL
