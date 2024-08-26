da4f5fa76fb191ebd2efa772591c7e59f91b200f
vinayakkatigar <47253535+vinayakkatigar@users.noreply.github.com>
2024-08-26 11:40:46 +0100

    26082024 GF Fix

--- src/main/java/stock/research/gfinance/email/alerts/GFinanceEmailAlertService.java
+++ src/main/java/stock/research/gfinance/email/alerts/GFinanceEmailAlertService.java
@@ -576,7 +576,8 @@ public class GFinanceEmailAlertService {
         stockInfoList.sort(comparing(GFinanceStockInfo::get_52WeekHighLowPriceDiff,
                 nullsFirst(naturalOrder())));
 
-        StringBuilder result = generateDailyEmail(stockInfoList, new StringBuilder("*** GF NYSE Daily Alerts ***"), false, sortByDailyPCTChange(stockInfoList));
+        StringBuilder result = generateDailyEmail(sortByDailyPCTChange(stockInfoList.stream().distinct().filter(x -> x.get_52WeekHighLowPriceDiff() != null && x.get_52WeekHighLowPriceDiff().doubleValue() <= 5d).collect(toList())),
+                new StringBuilder("*** GF NYSE Daily Alerts ***"), false, sortByDailyPCTChange(stockInfoList));
         LOGGER.info(" <-  Ended " + getClassName() + "::" + new Object() {}.getClass().getEnclosingMethod().getName());
         LOGGER.info(instantBefore.until(now(), SECONDS)+ " <- Total time in seconds, \nEnded "+
                 getClassName() + "::" + new Object() {}.getClass().getEnclosingMethod().getName());
