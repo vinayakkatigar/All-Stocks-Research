67a83557a397d3f3ee1a381f640f6b964ec8bcd5
vinayakkatigar <47253535+vinayakkatigar@users.noreply.github.com>
2024-09-09 09:16:25 +0100

    09092024 GF Fix

--- startStocks.bat
+++ startStocks.bat
@@ -1,11 +1,12 @@
 del /S /F /Q C:\Code\All-Stocks-Research\genHtml\*
 del /S /F /Q C:\Code\All-Stocks-Research\genHtml\*
+del /S /F /Q C:\Code\All-Stocks-Research\.git\index.lock
+del C:\Code\All-Stocks-Research\.git\index.lock
 
 rmdir /S /Q %temp%
 
 cd C:\Code\All-Stocks-Research
 c:
-
 cd C:\Code\All-Stocks-Research
 
 rmdir /S /Q C:\Code\All-Stocks-Research\logs
