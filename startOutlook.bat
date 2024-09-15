31475315bdfc7cc476e2f262ced447c3093afd4b
vinayakkatigar <47253535+vinayakkatigar@users.noreply.github.com>
2024-09-15 16:24:58 +0100

    15092024 GF Fix

--- startOutlook.bat
+++ startOutlook.bat
@@ -17,11 +17,11 @@ git reset --hard HEAD && git pull && git reset --hard HEAD && git pull && git re
 cd C:\Code\All-Stocks-Research
 c:
 cd C:\Code\All-Stocks-Research
-git reset --hard HEAD && git pull && git reset --hard HEAD && git pull && git reset --hard HEAD && git pull
+git reset --hard HEAD && git pull && git reset --hard HEAD && git pull && git reset --hard HEAD && git pull && git reset --hard HEAD && git pull
 
 cd C:\Code-Base\All-Stocks-Research
 c:
 cd C:\Code-Base\All-Stocks-Research
-git reset --hard HEAD && git pull && git reset --hard HEAD && git pull && git reset --hard HEAD && git pull
+git reset --hard HEAD && git pull && git reset --hard HEAD && git pull && git reset --hard HEAD && git pull && git reset --hard HEAD && git pull
 
 "C:\Program Files\Microsoft Office\root\Office16\OUTLOOK.EXE"
