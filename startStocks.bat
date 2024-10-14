99a72d8d78d05e65c95c08e4ba0d9cb51ac0c464
vinayakkatigar <47253535+vinayakkatigar@users.noreply.github.com>
2024-10-14 21:01:30 +0100

    14102024  Fix

--- startStocks.bat
+++ startStocks.bat
@@ -17,9 +17,7 @@ cd C:\Code\All-Stocks-Research
 c:
 cd C:\Code\All-Stocks-Research
 git reset --hard HEAD && git pull && git reset --hard HEAD && git pull && git reset --hard HEAD && git pull && git reset --hard HEAD && git pull && git reset --hard HEAD && git pull && git reset --hard HEAD && git pull
-git reset --hard HEAD && git pull && git reset --hard HEAD && git pull && git reset --hard HEAD && git pull && git reset --hard HEAD && git pull && git reset --hard HEAD && git pull && git reset --hard HEAD && git pull
 cd C:\Code\All-Stocks-Research
-git reset --hard HEAD && git pull && git reset --hard HEAD && git pull && git reset --hard HEAD && git pull && git reset --hard HEAD && git pull && git reset --hard HEAD && git pull && git reset --hard HEAD && git pull
 git reset --hard HEAD && git pull && git reset --hard HEAD && git pull && git reset --hard HEAD && git pull && git reset --hard HEAD && git pull && git reset --hard HEAD && git pull && git reset --hard HEAD && git pull
 
 mvn clean spring-boot:run
