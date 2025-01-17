5675628b977236a0eb87edfd473138d582b7b5f0
vinayakkatigar <47253535+vinayakkatigar@users.noreply.github.com>
2025-01-17 10:52:31 +0000

    17012025 fix

---
+++ startUpCommands.bat
@@ -0,0 +1,26 @@
+timeout /t 2 /nobreak > NUL
+wt -w 0 nt  C:\Code\All-Stocks-Research\startNotePad.bat
+
+timeout /t 5 /nobreak > NUL
+wt -w 0 nt  C:\Code\All-Stocks-Research\gFinanceAlive.bat
+
+timeout /t 10 /nobreak > NUL
+wt -w 0 nt C:\Vin\KeepAlive.bat
+
+timeout /t 10 /nobreak > NUL
+wt -w 0 nt  C:\Code\All-Stocks-Research\startStocks.bat
+
+timeout /t 2 /nobreak > NUL
+wt -w 0 nt  C:\Code\All-Stocks-Research\startOutlook.bat
+
+timeout /t 2 /nobreak > NUL
+wt -w 0 nt  C:\Code\All-Stocks-Research\gFinanceOneDayAlive.bat
+
+timeout /t 10 /nobreak > NUL
+wt -w 0 nt  C:\Code\yf-radar\yfRadarStartStocks.bat
+
+timeout /t 20 /nobreak > NUL
+wt -w 0 nt  C:\Code\yf-radar\yfRadarAlive.bat
+
+timeout /t 5 /nobreak > NUL
+wt -w 0 nt  C:\Code\All-Stocks-Research\enterKeyPresses.bat
