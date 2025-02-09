bf66914661dcd3af449e573b540b0ccd30a3b79c
vinayakkatigar <47253535+vinayakkatigar@users.noreply.github.com>
2025-02-09 09:51:15 +0000

    09022025 Fix

--- startNotePad.bat
+++ startNotePad.bat
@@ -8,7 +8,5 @@ c:
 cd C:\Code-Base\All-Stocks-Research
 git reset --hard HEAD && git pull && git reset --hard HEAD && git pull && git reset --hard HEAD && git pull
 
-xcopy C:\Code-Base\All-Stocks-Research\tokens C:\Code-Base\gf-radar\tokens /s /e /h  /F /R /Y /I
-xcopy C:\Code-Base\All-Stocks-Research\tokens C:\Code\gf-radar\tokens /s /e /h  /F /R /Y /I
 
 "C:\Program Files\Notepad++\notepad++.exe"
