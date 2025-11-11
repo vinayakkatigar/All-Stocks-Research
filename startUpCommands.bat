9a332b0428d285ea2860260ca3cc3ad09b1f5cbc
vinayakkatigar <47253535+vinayakkatigar@users.noreply.github.com>
2025-11-11 17:14:28 +0000

    11112025 Fix

--- startUpCommands.bat
+++ startUpCommands.bat
@@ -1,39 +1,39 @@
-cd C:\Code-Base\yf-radar-apac
+cd C:\Code\yf-radar
 git reset --hard HEAD && git pull && git reset --hard HEAD && git pull && git reset --hard HEAD && git pull && git reset --hard HEAD && git pull
 git reset --hard HEAD && git pull && git reset --hard HEAD && git pull && git reset --hard HEAD && git pull && git reset --hard HEAD && git pull
-cd C:\Code\yf-radar-apac
+cd C:\Code-Base\yf-radar
 git reset --hard HEAD && git pull && git reset --hard HEAD && git pull && git reset --hard HEAD && git pull && git reset --hard HEAD && git pull
 git reset --hard HEAD && git pull && git reset --hard HEAD && git pull && git reset --hard HEAD && git pull && git reset --hard HEAD && git pull
-cd C:\Code\All-Stocks-Research
+cd C:\Code-Base\All-Stocks-Research
 git reset --hard HEAD && git pull && git reset --hard HEAD && git pull && git reset --hard HEAD && git pull && git reset --hard HEAD && git pull
 git reset --hard HEAD && git pull && git reset --hard HEAD && git pull && git reset --hard HEAD && git pull && git reset --hard HEAD && git pull
 
 timeout /t 2 /nobreak > NUL
-wt -w 0 nt  C:\Code\All-Stocks-Research\startNotePad.bat
+wt -w 0 nt  C:\Code-Base\All-Stocks-Research\startNotePad.bat
 
 timeout /t 5 /nobreak > NUL
-wt -w 0 nt  C:\Code\All-Stocks-Research\gFinanceAlive.bat
+wt -w 0 nt  C:\Code-Base\All-Stocks-Research\gFinanceAlive.bat
 
 timeout /t 10 /nobreak > NUL
 wt -w 0 nt C:\Vin\KeepAlive.bat
 
 timeout /t 10 /nobreak > NUL
-wt -w 0 nt  C:\Code\All-Stocks-Research\startStocks.bat
+wt -w 0 nt  C:\Code-Base\All-Stocks-Research\startStocks.bat
 
 timeout /t 2 /nobreak > NUL
-wt -w 0 nt  C:\Code\All-Stocks-Research\startOutlook.bat
+wt -w 0 nt  C:\Code-Base\All-Stocks-Research\startOutlook.bat
 
 timeout /t 2 /nobreak > NUL
-wt -w 0 nt  C:\Code\All-Stocks-Research\gFinanceOneDayAlive.bat
+wt -w 0 nt  C:\Code-Base\All-Stocks-Research\gFinanceOneDayAlive.bat
 
 timeout /t 10 /nobreak > NUL
-wt -w 0 nt C:\Code\yf-radar-apac\yfRadarAPACStartStocks.bat
+wt -w 0 nt C:\Code-Base\yf-radar-apac\yfRadarAPACStartStocks.bat
 
 timeout /t 20 /nobreak > NUL
-wt -w 0 nt C:\Code\yf-radar-apac\yfAPACAlive.bat
+wt -w 0 nt C:\Code-Base\yf-radar-apac\yfAPACAlive.bat
 
 timeout /t 20 /nobreak > NUL
-REM wt -w 0 nt C:\Code\gf-radar\yfYearLowAlive.bat
+REM wt -w 0 nt C:\Code-Base\gf-radar\yfYearLowAlive.bat
 
 REM timeout /t 5 /nobreak > NUL
-REM wt -w 0 nt  C:\Code\All-Stocks-Research\enterKeyPresses.bat
+REM wt -w 0 nt  C:\Code-Base\All-Stocks-Research\enterKeyPresses.bat
