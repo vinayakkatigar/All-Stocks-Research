6e6d6a1d69577a5c135aef338087b9ee4d373f6c
vinayakkatigar <47253535+vinayakkatigar@users.noreply.github.com>
2026-02-07 03:56:34 +0000

    02022026 Fix

--- src/main/java/stock/research/controller/SensexController.java
+++ src/main/java/stock/research/controller/SensexController.java
@@ -9,8 +9,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import stock.research.email.alerts.SensexStockResearchAlertMechanismService;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+
+import static java.time.ZonedDateTime.now;
+
 @RestController
 public class SensexController {
+    public final static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd");
 
     @Autowired
     private SensexStockResearchAlertMechanismService stockResearchAlertMechanismService;
@@ -18,19 +26,45 @@ public class SensexController {
     @CrossOrigin
     @GetMapping("/screener/sensex/alerts")
     public String sensexPnL(){
-        return stockResearchAlertMechanismService.getSensexAlertsData();
+        String data = stockResearchAlertMechanismService.getSensexAlertsData();
+        writeHTMLFile((now().format(formatter))  + "SCREENER-SENSEX-ALERTS.html", data);
+        return data;
     }
 
     @CrossOrigin
     @GetMapping("/screener/sensex/daily")
     public String sensexDaily(){
-        return stockResearchAlertMechanismService.getSensexDailyData();
+        String data = stockResearchAlertMechanismService.getSensexDailyData();
+        writeHTMLFile((now().format(formatter))  + "SCREENER-SENSEX-DAILY.html", data);
+        return data;
     }
 
     @CrossOrigin
     @GetMapping("/screener/sensex/yearlow")
     public String sensexYearLow(){
-        return stockResearchAlertMechanismService.getYearLow();
+        String data = stockResearchAlertMechanismService.getYearLow();
+        writeHTMLFile((now().format(formatter))  + "SCREENER-SENSEX-YEAR-LOW.html", data);
+        return data;
+    }
+
+    public String getDateFileFormat(String input){
+        int sum = 0;
+        for(int i = 0; i < input.length(); i++) {
+            if(Character.isDigit(input.charAt(i))) {
+                sum = sum + Integer.parseInt(input.charAt(i) + "");
+            }
+        }
+        return (sum * ZonedDateTime.now().getMonthValue()) + "--" + input;
+    }
+
+    public static void writeHTMLFile(String fileName, String fileContent, String... subDir){
+        try {
+            String fileDir = "\\genHtml\\";
+            if (subDir != null && subDir.length > 0) {
+                fileDir = fileDir + "\\" + subDir[0]  + "\\"  ;
+            }
+            Files.write(Paths.get(System.getProperty("user.dir") + fileDir + fileName), fileContent.getBytes());
+        } catch (Exception | Error e) { }
     }
 
 }
