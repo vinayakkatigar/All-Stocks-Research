5c71616ab75204fea2b819a8a7197fedbff4867e
vinayakkatigar <47253535+vinayakkatigar@users.noreply.github.com>
2026-02-19 14:03:01 +0000

    19022026 Fix

--- gFinanceAlive.bat
+++ gFinanceAlive.bat
@@ -42,6 +42,12 @@ timeout /t 30 /nobreak > NUL
 REM powershell.exe  C:\Code-Base\enterKeyPress.ps1
 timeout /t 10 /nobreak > NUL
 start chrome https://docs.google.com/spreadsheets/d/1DdkJYnXIR0UCLeB7cjB8G4LGzZMXHKQ_dGpXGO8CU8I/edit#gid=0 && start chrome https://docs.google.com/spreadsheets/d/1YmgSZuLMPJqgPLUuaQCTq2TXjts4aPD0w19zYpV0WpE/edit#gid=0 && start chrome https://docs.google.com/spreadsheets/d/1TGdtwdz_6O9wTO3xFzUwo4wlbsyGsQWeHolLVJzxLyQ/edit#gid=0 && start chrome https://docs.google.com/spreadsheets/d/1M7swFwopiNGRZn052yCc1YM3cYVQvLiBytJNehncSPI/edit#gid=0 && start chrome https://docs.google.com/spreadsheets/d/1wjfxknOyQ5TghGwoFOt-LTz2E-APPXFAfsYqaooIyBQ/edit#gid=0 && start chrome https://docs.google.com/spreadsheets/d/1j6QRSY9A9b8hYWBEidkl5B4X6FaiVByS2tUc4aU4b6M/edit#gid=0 && start chrome --new-window https://docs.google.com/spreadsheets/d/1Hvw1BJH1eJWbtJwReD3n96DGeHarFVXeR93B3-0tKPE/edit#gid=0 && start chrome https://docs.google.com/spreadsheets/d/1WSVRVhqU6vUdatoic49kwdAUSoBUaOlqZscyTVzX8CE/edit#gid=0 && start chrome https://docs.google.com/spreadsheets/d/1YlVMw15EIi2-Z62TIJ3Bt6-zb0UlR_4FZrjc1LoPnHU/edit#gid=0 && start chrome https://docs.google.com/spreadsheets/d/1iF_6oxXe2bvyQCNadAUlaLB_3GSkhNrzik9HZyK2iUI/edit#gid=0 && start chrome https://docs.google.com/spreadsheets/d/1gqq8oNpG35WwhSARwIoFhTQOs2yqVa6UPzTTzLgAdew/edit#gid=0 && start chrome https://docs.google.com/spreadsheets/d/1qijUJ91J-Qzuj2wI9RdWULOoq1XaZQMrAZOpWwbyt3Y/edit#gid=0 && start chrome https://docs.google.com/spreadsheets/d/1V89w-xI5urpoBIcCAHeIoho0J1cJxkFzSQXmG04U85w/edit#gid=0 && start chrome https://docs.google.com/spreadsheets/d/1cOJOjVE49DCjFFMq7JFdmDeOFcf3MzwV2hl6K7gyX9g/edit#gid=0 && start chrome https://docs.google.com/spreadsheets/d/1FybDb-TiZ1T10HUDxwVoWvQj2WHAQZG5RWqzQUX2MTI/edit#gid=0 && start chrome https://docs.google.com/spreadsheets/d/1q4PG03AHihCXg1wGHO6bKYaczyOxqW-T0BgUjJ4axJo/edit#gid=0
+timeout /t 15 /nobreak > NUL
+start chrome https://docs.google.com/spreadsheets/d/1DTBXi_iUkaSUK7u0YF0ETeK2wJRbKgTCSEH8QL3FUA8/edit?gid=0#gid=0
+start chrome https://docs.google.com/spreadsheets/d/10iBogki4CClWmeoTKcena1hQ1ogh7n9_MD4rTg1CH9c/edit?gid=0#gid=0
+start https://docs.google.com/spreadsheets/d/1ki_vTyELjBXvtfrJphpdkHmeqx-FCSQKrwYPwDXVvEE/edit?gid=0#gid=0
+start https://docs.google.com/spreadsheets/d/1nL6tdOfqITQzcRzdRFCIpHn2ddpQ23iCl14nWxbmWiU/edit?gid=0#gid=0
+
 timeout /t 45 /nobreak > NUL
 
 start chrome --new-window https://docs.google.com/spreadsheets/d/14uokCiL9lYv4eRYbJbi8QRsgkGi4d2nUwqQP-bTZ3fI/edit#gid=0
